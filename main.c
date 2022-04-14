9be80cf6b4c0458d74fc84833984010e524f71f5
CaoJun <casojun@outlook.com>
2022-04-14 21:52:04 +0800

    测试修改

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include "test.h"
+
 int main()
 {
-    int a = 0;
-    printf("CaoJun\n");
-    printf("%d\n", a); // alt+sheift+F
-    printf_caojun();
+
     return 0;
 }
--- test.c
+++
@@ -1,6 +0,0 @@
-#include "test.h"
-
-void printf_caojun()
-{
-    printf("test.h\n");
-}
