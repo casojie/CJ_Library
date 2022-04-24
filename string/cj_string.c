51366c80dff4780b84f56388ae3b4496779de4d2
CaoJie <casojie@casojie.top>
2022-04-24 09:43:09 +0800

    新增实现字符串合并函数

--- string/cj_string.c
+++ string/cj_string.c
@@ -2,7 +2,7 @@
  * @Author       : CaoJie
  * @Date         : 2022-04-20 12:10:23
  * @LastEditors  : CaoJie
- * @LastEditTime : 2022-04-20 14:38:56
+ * @LastEditTime : 2022-04-24 09:41:19
  * @Description  : file content
  */
 #include "cj_string.h"
@@ -60,16 +60,78 @@ void stringSort(char *string, unsigned int stringLen, short mode)
         }
     }
 }
+
 /**
- * @Descripttion: 将desString 添加到srcString中，请确保srcString有足够的空间存储
- * @param {char} *srcString 源字符串
- * @param {char} *desString 目标字符串
- * @param {unsigned int} srcStringLen 源字符串长度
- * @param {unsigned int} desStringLen 目标字符串长度
- * @param {unsigned int} index 插入下标
- * @return {*}
+ * @Descripttion: 对字符串进行插入合并操作产生新的字符串
+ * @param {char} *desString 插入目标string
+ * @param {char} *srcString 插入内容
+ * @param {int} desStringLen 插入目标string原本有效长度
+ * @param {int} maxDesStringLen 目标string最大能存储的字节数
+ * @param {int} srcStringLen 插入内容长度
+ * @param {int} index 插入位置下标
+ * @param {short} mode 是否允许使用relloc 对desString进行扩容，由malloc等函数生成的destring可以使用 0:不允许 1：允许
+ * @param {char} ** 返回合并完成的指针，原来desstring可以抛弃使用
+ * @return {int} 合并后的长度
  */
-void appendString(char *srcString, char *desString, unsigned int srcStringLen, unsigned int desStringLen, unsigned int index)
+int appendString(char *desString, char *srcString, int desStringLen, int maxDesStringLen, int srcStringLen, int index, short mode, char **newStringPinter)
 {
-    
+    const char *TAG = "appendString";
+
+    char *newString = NULL;
+
+    if (NULL == desString || NULL == srcString)
+    {
+        printf("[%s]: pointer is NULL\n", TAG);
+        return -1;
+    }
+
+    unsigned int srcStringLenRet = 0; //合并后长度
+    //计算合并后的长度, 先不考虑maxDesStringLen
+    if (index <= desStringLen)
+    {
+        srcStringLenRet = desStringLen + srcStringLen;
+    }
+    else
+    {
+        srcStringLenRet = index + srcStringLen;
+    }
+
+    //是否需要重新分配内存
+    if (1 == mode) // malloc生成的指针
+    {
+        if (srcStringLenRet > maxDesStringLen) //所需长度已大于des原本最大存储长度, 需要重新分配长度
+        {
+            newString = (char *)realloc(desString, srcStringLenRet * sizeof(char));
+            if (NULL == newString)
+            {
+                printf("[%s]: realloc return NULL\n", TAG);
+                return -2;
+            }
+            memset(newString + maxDesStringLen, 0, srcStringLenRet - maxDesStringLen); //将新多出的空间初始化
+            maxDesStringLen = srcStringLenRet;
+        }
+    }
+    else
+    {
+        newString = desString;
+        if (srcStringLenRet > maxDesStringLen)
+        {
+            printf("[%s]: out of srcString array\n", TAG);
+            return -3;
+        }
+    }
+
+    // 合并字符串
+    if (index <= maxDesStringLen)
+    {
+        int pos = 0;
+        for (pos = maxDesStringLen - 1 - srcStringLen; pos >= index; pos--)
+        {
+            newString[pos + srcStringLen] = newString[pos];
+        }
+    }
+    memcpy(newString + index, srcString, srcStringLen);
+    *newStringPinter = newString; //返回新创字符串指针
+
+    return srcStringLenRet;
 }
--- string/cj_string.h
+++ string/cj_string.h
@@ -2,7 +2,7 @@
  * @Author       : CaoJie
  * @Date         : 2022-04-15 11:31:24
  * @LastEditors  : CaoJie
- * @LastEditTime : 2022-04-19 16:57:10
+ * @LastEditTime : 2022-04-24 09:42:03
  * @Description  : file content
  */
 #ifndef _CJ_STRING_H_
@@ -19,13 +19,16 @@
  */
 void stringSort(char *string, unsigned int stringLen, short mode);
 /**
- * @Descripttion: 将desString 添加到srcString中，请确保srcString有足够的空间存储
- * @param {char} *srcString 源字符串
- * @param {char} *desString 目标字符串
- * @param {unsigned int} srcStringLen 源字符串长度
- * @param {unsigned int} desStringLen 目标字符串长度
- * @param {unsigned int} index 插入下标
- * @return {*}
+ * @Descripttion: 对字符串进行插入合并操作产生新的字符串
+ * @param {char} *desString 插入目标string
+ * @param {char} *srcString 插入内容
+ * @param {int} desStringLen 插入目标string原本有效长度
+ * @param {int} maxDesStringLen 目标string最大能存储的字节数
+ * @param {int} srcStringLen 插入内容长度
+ * @param {int} index 插入位置下标
+ * @param {short} mode 是否允许使用relloc 对desString进行扩容，由malloc等函数生成的destring可以使用 0:不允许 1：允许
+ * @param {char} ** 返回合并完成的指针，原来desstring可以抛弃使用
+ * @return {int} 合并后的长度
  */
-void appendString(char *srcString, char *desString, unsigned int srcStringLen, unsigned int desStringLen, unsigned int index);
+int appendString(char *desString, char *srcString, int desStringLen, int maxDesStringLen, int srcStringLen, int index, short mode, char **newStringPinter)
 #endif
